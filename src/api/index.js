8e0a3d0e3142ef4ee607154707a01d7c0a3db2bc
Daniel Muxel <daniel.muxel@gmail.com>
2023-06-16 18:53:50 +0200

    added server message that says the server is running

--- src/api/index.js
+++ src/api/index.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); // Middleware for JSON data
 app.use("/api/todos", todoRoutes);
 
+// add a route on / that says the server is up and running
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+
 // eslint-disable-next-line no-console
 app.listen(PORT, () =>
   console.log(`Server started on port http://localhost:${PORT}/`)
